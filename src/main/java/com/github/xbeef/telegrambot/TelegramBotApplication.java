541502e90deb2b74b84c1ee3c00bdeacee90f8b4
xbeef <49283042+xbeef@users.noreply.github.com>
2024-07-22 01:22:37 +0300

    JRTB-2: add stub telegram bot to the project (#9)
    * JRTB-2: add stub telegram bot to the project
    
    * JRTB-2: update project version and RELEASE_NOTES.md

--- src/main/java/com/github/xbeef/telegrambot/TelegramBotApplication.java
+++ src/main/java/com/github/xbeef/telegrambot/TelegramBotApplication.java
@@ -2,8 +2,13 @@ package com.github.xbeef.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.github.xbeef.telegrambot",
+		"org.telegram.telegrambots"
+})
 public class TelegramBotApplication {
 
 	public static void main(String[] args) {
